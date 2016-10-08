57f7b7783394b05973bd7ea742dc81709d2c6ac3
estermer <eric.stermer@gmail.com>
2016-10-08 15:22:25 -0400

    Styled new run form

--- controllers/new-run.js
+++ controllers/new-run.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.render('new');
+  res.render('new', {
+    username: req.user.username
+  });
 });
 
 module.exports = router;
