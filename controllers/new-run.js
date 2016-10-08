5ed7a20c9430153cdd62bd392e3d841304534278
estermer <eric.stermer@gmail.com>
2016-10-08 15:31:20 -0400

    Added a post route to take in new run submitted data

--- controllers/new-run.js
+++ controllers/new-run.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//RENDER NEW RUN FORM
 router.get('/', function(req, res){
   res.render('new', {
     username: req.user.username
   });
+});
+
+//SUBMIT NEW RUN INFO TO DB
+router.post('/create', function(req, res){
+  console.log(req.body);
+  res.redirect('/' + req.user.username);
 });
 
 module.exports = router;
