7a3cb9c875ee4f63b756a15945e7a4d4de415c1f
estermer <eric.stermer@gmail.com>
2016-10-08 21:48:53 -0400

    added an edit run route and htm for the edit run view with the form

--- controllers/edit-run.js
+++ controllers/edit-run.js
@@ -1,5 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user.js').model;
+var logic = require('../public/js/logic.js');
+
+router.get('/', function(req, res){
+  if (!req.user) {
+    res.redirect('/');
+  } else {
+
+    User.findOne({username: req.user.username}, function(err, user){
+      var runDetails = logic.findRun(user.runningLog, req.params.id);
+
+      res.render('show', {
+        username: req.user.username,
+        runName: runDetails.runName,
+        date: runDetails.date,
+        distance: runDetails.distance,
+        time: runDetails.time,
+        pace: runDetails.pace,
+        rpe: runDetails.rpe,
+        description: runDetails.description
+      });
+    });
+  }
+});
 
 
 
